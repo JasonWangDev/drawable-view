520a2085c0cef73519f3134b47afe5498b3a9ebb
Jason Wang <test@gmail.com>
2017-07-08 21:07:32 +0800

    Modify InstantAutoCompleteTextView Keyboard behavior & Add canInput function

--- app/src/main/java/com/github/jasonwangdev/drawableview/demo/MainActivity.java
+++ app/src/main/java/com/github/jasonwangdev/drawableview/demo/MainActivity.java
@@ -6,20 +6,26 @@ import android.widget.ArrayAdapter;
 
 import com.github.jasonwangdev.drawableview.DrawableInstantAutoCompleteTextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    private String[] datas = {"SEGSEG", "23234", "測試設", "sveb,,e4,"};
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        List<String> datas = new ArrayList<>();
+        for (int i = 0 ; i < 20000 ; i++)
+            datas.add("Item " + i);
+        
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                                                           android.R.layout.simple_dropdown_item_1line,
                                                           datas);
 
         DrawableInstantAutoCompleteTextView tv = (DrawableInstantAutoCompleteTextView) findViewById(R.id.tv);
+        tv.setCanInput(false);
         tv.setAdapter(adapter);
     }
 
--- drawable-view-library/src/main/java/com/github/jasonwangdev/drawableview/super_view/InstantAutoCompleteTextView.java
+++ drawable-view-library/src/main/java/com/github/jasonwangdev/drawableview/super_view/InstantAutoCompleteTextView.java
@@ -2,34 +2,91 @@ package com.github.jasonwangdev.drawableview.super_view;
 
 import android.content.Context;
 import android.graphics.Rect;
+import android.os.Build;
+import android.text.InputType;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AutoCompleteTextView;
+import android.widget.ListPopupWindow;
+import android.widget.PopupWindow;
+
+import java.lang.reflect.Field;
 
 
 /**
+ *
+ * 純下拉(不能輸入)
+ * 建議下拉(根據輸入的內容給予建議資料)
+ *
  * Created by jason on 2017/7/5.
  */
 
-public class InstantAutoCompleteTextView extends android.support.v7.widget.AppCompatAutoCompleteTextView implements View.OnClickListener {
+public class InstantAutoCompleteTextView extends android.support.v7.widget.AppCompatAutoCompleteTextView {
+
+    /*************************************** 私有介面物件宣告 ***************************************/
+
+    private OnClickListener onClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            showDropDownIfFocused();
+        }
+    };
+
+    private OnDismissListener onDismissListenerForApi17 = new OnDismissListener() {
+        @Override
+        public void onDismiss() { }
+    };
+
+    private PopupWindow.OnDismissListener onDismissListener = new PopupWindow.OnDismissListener() {
+        @Override
+        public void onDismiss() { }
+    };
+
+
+    /******************************************** 建構子 ********************************************/
 
     public InstantAutoCompleteTextView(Context context) {
         super(context);
 
-        setOnClickListener(this);
+        init();
     }
 
     public InstantAutoCompleteTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        setOnClickListener(this);
+        init();
     }
 
     public InstantAutoCompleteTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
-        setOnClickListener(this);
+        init();
     }
 
+
+    /******************************************* 生命週期 *******************************************/
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        registerOnClickListener();
+        registerOnDismissListener();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        unregisterOnCLickListener();
+        unregisterOnDismissListener();
+
+        super.onDetachedFromWindow();
+    }
+
+
+    /******************************************* Callback ********************************************/
 
     @Override
     public boolean enoughToFilter() {
@@ -44,15 +101,86 @@ public class InstantAutoCompleteTextView extends android.support.v7.widget.AppCo
     }
 
     @Override
-    protected void onAttachedToWindow() {
-        super.onAttachedToWindow();
+    public boolean onKeyPreIme(int keyCode, KeyEvent event) {
+        // 當 Back 鍵被點擊
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_UP)
+        {
+            // 確認列表視窗已顯示
+            if (isPopupShowing())
+            {
+                // 關閉軟體鍵盤，並停止傳送 onKeyPreIme 事件
+                // P.S. 預設處理事件是按下 Back 鍵後先關閉列表視窗(我們想要的效果是先關閉鍵盤)
+                InputMethodManager inputManager = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (inputManager.hideSoftInputFromWindow(findFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS))
+                    return true;
+            }
+        }
 
-        showDropDownIfFocused();
+        return super.onKeyPreIme(keyCode, event);
     }
 
-    @Override
-    public void onClick(View view) {
-        showDropDownIfFocused();
+    /******************************************* 公有方法 *******************************************/
+
+    public void setCanInput(boolean canInput) {
+        if (!canInput)
+            setInputType(InputType.TYPE_NULL);
+        else
+            setInputType(InputType.TYPE_CLASS_TEXT);
+    }
+
+
+    /******************************************* 私有方法 *******************************************/
+
+    private void init() {
+        registerOnDismissListener();
+        registerOnClickListener();
+
+        setMaxLines(1);
+        setSingleLine(true);
+    }
+
+
+    private void registerOnClickListener() {
+        setOnClickListener(onClickListener);
+    }
+
+    private void unregisterOnCLickListener() {
+        setOnClickListener(null);
+    }
+
+
+    private void registerOnDismissListener() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
+            setOnDismissListener(onDismissListenerForApi17);
+        else
+            setOnDismissListenerByReflection(onDismissListener);
+    }
+
+    private void unregisterOnDismissListener() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
+            setOnDismissListener(null);
+        else
+            setOnDismissListenerByReflection(null);
+    }
+
+
+    private void setOnDismissListenerByReflection(PopupWindow.OnDismissListener listener) {
+        try
+        {
+            Field field = AutoCompleteTextView.class.getDeclaredField("mPopup");
+            field.setAccessible(true);
+
+            ListPopupWindow popupWindow = (ListPopupWindow) field.get(this);
+            popupWindow.setOnDismissListener(listener);
+        }
+        catch (NoSuchFieldException e)
+        {
+            Log.e("TAG", e.getMessage());
+        }
+        catch (IllegalAccessException e)
+        {
+            Log.e("TAG", e.getMessage());
+        }
     }
 
 
